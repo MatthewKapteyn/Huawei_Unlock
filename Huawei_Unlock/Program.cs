5f84c3e4213221fd1d80f6ad2c8cc0a4c250897f
Matthew Kapteyn <matthewk@maple.ca>
2017-09-20 09:37:22 -0400

    First commit of dummy project

---
+++ Huawei_Unlock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Huawei_Unlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Debug.WriteLine(Test.HW_ALGO_SELECTOR("This_is_a_15_digit_imei_jk"));
+        }
+
+        
+    }
+}
---
+++ Huawei_Unlock/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huawei_Unlock
+{
+    public class Test
+    {
+        public static long HW_ALGO_SELECTOR(string IMEI)
+        {
+            string Code = "";
+
+            // No idea if this works - it's something I toyed with for 5 minutes and never tested
+            for (int i = 0; i < 15; i++)
+            {
+                Code = Code + ((int)IMEI[i] + (i + 1)) * (i + 1);
+            }
+
+            long RealCode = Int64.Parse(Code);
+
+            return (RealCode % 7);
+        }
+    }
+}
